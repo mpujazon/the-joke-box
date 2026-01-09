73b928e953d39c5f82f0413a7d0fda04f2eb19e0
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-01-10 00:30:21 +0100

    fetchChuckJoke() Function Implemented

--- src/jokes/jokes.api.ts
+++ src/jokes/jokes.api.ts
@@ -1,4 +1,4 @@
-import type { DadJokeData } from "../types";
+import type { ChuckJokeData, DadJokeData } from "../types";
 
 export const fetchDadJoke = async (): Promise<DadJokeData> => {
     const url = 'https://icanhazdadjoke.com/';
@@ -8,6 +8,18 @@ export const fetchDadJoke = async (): Promise<DadJokeData> => {
         throw new Error(`Response status: ${response.status}`);
     }
 
-    const data = response.json();
+    const data = await response.json();
+    return data;
+}
+
+export const fetchChuckJoke = async (): Promise<ChuckJokeData> => {
+    const url = 'https://api.chucknorris.io/jokes/random';
+    const response = await fetch(url);
+
+    if (!response.ok){
+        throw new Error(`Response status: ${response.status}`);
+    }
+
+    const data = await response.json();
     return data;
 }
