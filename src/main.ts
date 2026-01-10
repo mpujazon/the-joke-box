28c67f7475e193941f1cfcca86832069009e879c
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-01-10 18:14:13 +0100

    main.ts fixed

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { loadWeather } from "./weather/weather.logic"
 
 const init = async() => {
     try {
-        // const [joke, weather] = await Promise.allSettled([
-        //     loadJoke(), 
-        //     loadWeather()
-        // ]).then(results => results.map( result=>
-        //     result.status === 'fulfilled'? result.value : null
-        // ));
-        //renderJoke(joke);
-        //renderWeather(weather);
+        const [joke, weather] = await Promise.allSettled([
+            loadJoke(), 
+            loadWeather()
+        ]).then(results => results.map( result=>
+            result.status === 'fulfilled'? result.value : null
+        ));
+        // renderJoke(joke);
+        // renderWeather(weather);
     } catch (error) {
         console.error("Error:", error);
     }
